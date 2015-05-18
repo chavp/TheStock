f331a62da3e2e09366fb8f4d00837cc7052cda89
chavp <my.parinya@gmail.com>
2015-05-18 17:23:53 +0700

    add exebot message
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ ExebotDev/ThePortfolioConcept/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThePortfolioConcept.Startup))]
+namespace ThePortfolioConcept
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
